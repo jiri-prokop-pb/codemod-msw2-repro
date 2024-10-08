f4394a626301474006eee15897940fffb3940c0a
Jiří Prokop <jiri.prokop@productboard.com>
2024-10-08 11:58:45 +0200

    Improve upgrade-msw-v2.sh script

--- upgrade-msw-v2.sh
+++ upgrade-msw-v2.sh
@@ -1,2 +1,12 @@
 #!/usr/bin/env bash
-pnpx codemod msw/2/upgrade-recipe
+
+echo "Upgrading to MSW v2: First run without --logs flag"
+set -x
+pnpx codemod msw/2/upgrade-recipe --no-interactive --include test.spec.ts
+set +x
+
+echo "###############################################"
+echo "Upgrading to MSW v2: First run with --logs flag"
+set -x
+pnpx codemod msw/2/upgrade-recipe --no-interactive --include test.spec.ts --logs
+set +x
